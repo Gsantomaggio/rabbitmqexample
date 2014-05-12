31ead7de65ddf0b8aed1b31ab59dcab5816af524
gabriele <g.santomaggio@gmail.com>
2014-05-12 21:08:34 +0200

    logging

--- webSocketPython/my-server.py
+++ webSocketPython/my-server.py
@@ -12,18 +12,19 @@ import tornado.websocket
 import pika
 from threading import Thread
 import logging
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
+
 
 # web socket clients connected.
 clients = []
 
 connection = pika.BlockingConnection()
-print 'Connected:localhost'
+logging.info('Connected:localhost')
 channel = connection.channel()
 
 def threaded_rmq():
 	channel.queue_declare(queue="my_queue")
-	print 'consumer ready, on my_queue'
+	logging.info('consumer ready, on my_queue')
 	channel.basic_consume(consumer_callback, queue="my_queue", no_ack=True) 
 	channel.start_consuming()
 	
@@ -31,22 +32,22 @@ def threaded_rmq():
 def disconnect_to_rabbitmq():
 	channel.stop_consuming()
 	connection.close()
-	print 'Disconnected from Rabbitmq'
+	logging.info('Disconnected from Rabbitmq')
 
 	
 def consumer_callback(ch, method, properties, body):
-		print " [x] Received %r" % (body,)
+		logging.info("[x] Received %r" % (body,))
 		# The messagge is brodcast to the connected clients
 		for itm in clients:
 			itm.write_message(body)
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
-		print "WebSocket opened"
+		logging.info('WebSocket opened')
 		clients.append(self)
 	
 	def on_close(self):
-		print "WebSocket closed"
+		logging.info('WebSocket closed')
 		clients.remove(self)
 
 
@@ -68,25 +69,25 @@ def stopTornado():
     tornado.ioloop.IOLoop.instance().stop()
 
 if __name__ == "__main__":
-	print "Starting thread RabbitMQ "
+	logging.info('Starting thread RabbitMQ')
 	threadRMQ = Thread(target=threaded_rmq)
 	threadRMQ.start()
 
-   	print "Starting thread Tornado"
+   	logging.info('Starting thread Tornado')
 
    	threadTornado = Thread(target=startTornado)
    	threadTornado.start()
 	try:
-		raw_input("Press enter to stop\n")
+		raw_input("Server ready. Press enter to stop\n")
 	except SyntaxError:
 		pass
 	try:
-		print "Disconnecting from RabbitMQ.."
+		logging.info('Disconnecting from RabbitMQ..')
 		disconnect_to_rabbitmq()
 	except Exception, e:
 		pass
 	stopTornado(); 
 	
-	print 'See you...'
+	logging.info('See you...')
 	
 	
